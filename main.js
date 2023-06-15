30f41aa630886b710f42a477a90af08b1fb5fc5f
naazpreetgiani <124805700+naazpreetgiani@users.noreply.github.com>
2023-06-15 09:24:05 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//INPUT
+let username = prompt("What is your name?");
+let TimeOfDay = prompt("What time of day is it?");
+
+
+//PROCESS
+let msg = `Hello ${username}! Have a great ${TimeOfDay}.`;
+
+//OUTPUT
+alert(msg);
